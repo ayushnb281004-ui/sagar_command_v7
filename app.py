bd5e33cf67e452b4e978913c53f7146f8b059234
ayushnb281004-ui <ayushnb.281004@gmail.com>
2026-04-24 09:16:06 +0530

    Add reload button to refresh sensor data

--- app.py
+++ app.py
@@ -1,7 +1,1 @@
-# --- RELOAD CONTROLS ---
-reload_col1, reload_col2 = st.columns([1, 5])
-with reload_col1:
-    if st.button("🔄 RELOAD DATA", use_container_width=True):
-        st.rerun()
-
-st.divider()
+Current Date and Time (UTC - YYYY-MM-DD HH:MM:SS formatted): 2026-04-24 03:44:44
